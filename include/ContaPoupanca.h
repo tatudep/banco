761d7641e5119dbb98b55466b7d97ea9638e4263
tatudep <167444133+tatudep@users.noreply.github.com>
2025-02-04 10:25:00 -0300

    Update ContaPoupanca.h

--- include/ContaPoupanca.h
+++ include/ContaPoupanca.h
@@ -1,15 +1,19 @@
 #ifndef CONTAPOUPANCA_H
 #define CONTAPOUPANCA_H
 
-#include "Conta.h"
+#include "Conta.h" // Inclui a definição da classe Conta
 
+// Classe que representa uma conta poupança, que é um tipo de conta bancária
 class ContaPoupanca : public Conta {
 private:
-    double taxaRendimento;
+    double taxaRendimento; // Porcentagem de rendimento da poupança
 
 public:
+    // Construtor que cria uma conta poupança com um ID e uma taxa de rendimento específica
     ContaPoupanca(int id, double taxaRendimento);
-    void aplicarRendimento(); // Método específico para conta poupança
+
+    // Método que aplica o rendimento ao saldo da conta, baseado na taxa definida
+    void aplicarRendimento();
 };
 
-#endif
+#endif // Finaliza a definição da classe ContaPoupanca
