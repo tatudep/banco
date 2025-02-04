6890b3e5fe64e4b2454f375ebbbc481a0c843c30
tatudep <167444133+tatudep@users.noreply.github.com>
2025-02-04 10:32:18 -0300

    Update ContaPoupanca.cpp

--- src/ContaPoupanca.cpp
+++ src/ContaPoupanca.cpp
@@ -1,11 +1,13 @@
 #include "ContaPoupanca.h"
 #include <iostream>
 
+// Construtor da ContaPoupanca, recebe um ID e a taxa de rendimento da conta
 ContaPoupanca::ContaPoupanca(int id, double taxaRendimento)
-    : Conta(id), taxaRendimento(taxaRendimento) {}
+    : Conta(id), taxaRendimento(taxaRendimento) {} // Chama o construtor da classe base (Conta) e define a taxa de rendimento
 
+// Método que aplica o rendimento sobre o saldo da conta
 void ContaPoupanca::aplicarRendimento() {
-    double rendimento = saldo * taxaRendimento;
-    saldo += rendimento;
-    transacoes.emplace_back("Rendimento", rendimento); // Cria um objeto Transacao
+    double rendimento = saldo * taxaRendimento; // Calcula o valor do rendimento com base no saldo atual
+    saldo += rendimento; // Adiciona o rendimento ao saldo da conta
+    transacoes.emplace_back("Rendimento", rendimento); // Registra a transação do rendimento
 }
